30f5cf8b2cb61b0eadaffe77a7cdf7abbffbbb25
souravmishra7456 <mishra.sourav79@gmail.com>
2025-10-20 07:17:35 +0530

    baseURL changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,25 @@
 /* eslint-disable no-unused-vars */
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import IndexPage from './pages/IndexPage'
-import RegisterPage from './pages/RegisterPage'
-import Layout from './Layout'
-import LoginPage from './pages/LoginPage'
-import axios from 'axios'
-import { UserContextProvider } from './UserContext'
-import UserAccountPage from './pages/UserAccountPage'
-import ForgotPassword from './pages/ForgotPassword'
-import ResetPassword from './pages/ResetPassword'
-import AddEvent from './pages/AddEvent'
-import EventPage from './pages/EventPage'
-import CalendarView from './pages/CalendarView'
-import OrderSummary from './pages/OrderSummary'
-import PaymentSummary from './pages/PaymentSummary'
-import TicketPage from './pages/TicketPage'
-import CreatEvent from './pages/CreateEvent'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import IndexPage from "./pages/IndexPage";
+import RegisterPage from "./pages/RegisterPage";
+import Layout from "./Layout";
+import LoginPage from "./pages/LoginPage";
+import axios from "axios";
+import { UserContextProvider } from "./UserContext";
+import UserAccountPage from "./pages/UserAccountPage";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import AddEvent from "./pages/AddEvent";
+import EventPage from "./pages/EventPage";
+import CalendarView from "./pages/CalendarView";
+import OrderSummary from "./pages/OrderSummary";
+import PaymentSummary from "./pages/PaymentSummary";
+import TicketPage from "./pages/TicketPage";
+import CreatEvent from "./pages/CreateEvent";
 
-axios.defaults.baseURL = 'http://localhost:4000/';
-axios.defaults.withCredentials=true;
+axios.defaults.baseURL = "https://eventoems-lnea.onrender.com";
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
@@ -49,4 +49,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
